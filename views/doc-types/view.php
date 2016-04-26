48617b58ed8f0d0ea1c7888954eae158aebc475d
Sergey Okatov <sokatov@gmail.com>
2016-04-26 13:16:30 +0000

    work on treeview in statuses

--- views/doc-types/view.php
+++ views/doc-types/view.php
@@ -1,8 +1,15 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+
 use yii\widgets\DetailView;
+use yii\widgets\Pjax;
+
 use yii\grid\GridView;
+
+use yii\web\JsExpression;
+use execut\widget\TreeView;
 
 /* @var $this yii\web\View */
 /* @var $model statuses\models\DocTypes */
@@ -57,4 +64,66 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ],
     ]); ?>
+
+<?php
+Pjax::begin([
+    'id' => 'pjax-container',
+]);
+
+echo \yii::$app->request->get('page');
+
+Pjax::end();
+
+$onSelect = new JsExpression(<<<JS
+function (undefined, item) {
+    if (item.href !== location.pathname) {
+        $.pjax({
+            container: '#pjax-container',
+            url: item.href,
+            timeout: null
+        });
+    }
+
+    var otherTreeWidgetEl = $('.treeview.small').not($(this)),
+        otherTreeWidget = otherTreeWidgetEl.data('treeview'),
+        selectedEl = otherTreeWidgetEl.find('.node-selected');
+    if (selectedEl.length) {
+        otherTreeWidget.unselectNode(Number(selectedEl.attr('data-nodeid')));
+    }
+}
+JS
+);
+
+$items = [
+    [
+        'text' => 'Parent 1',
+        'href' => Url::to(['', 'page' => 'parent1']),
+        'nodes' => [
+            [
+                'text' => 'Child 1',
+                'href' => Url::to(['', 'page' => 'child1']),
+                'nodes' => [
+                    [
+                        'text' => 'Grandchild 1',
+                        'href' => Url::to(['', 'page' => 'grandchild1'])
+                    ],
+                    [
+                        'text' => 'Grandchild 2',
+                        'href' => Url::to(['', 'page' => 'grandchild2'])
+                    ]
+                ]
+            ],
+        ],
+    ],
+];
+
+echo TreeView::widget([
+    'data' => $items,
+    'size' => TreeView::SIZE_SMALL,
+    'clientOptions' => [
+        'onNodeSelected' => $onSelect,
+    ],
+]);
+?>
+
 </div>
